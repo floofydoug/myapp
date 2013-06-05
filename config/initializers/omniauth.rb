73ebe89b38504ff421f40eaba5e6b7a8185d7ebe
Doug Phung <Doug@chillportal.com>
2013-06-05 16:57:20 -0400

    added tweetstream for a given keyword

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :twitter,'YX4uQcPXYbqhY0CTEee1cA' , 'mDF4D1ui0OSIXP1dcT22YZRvD2lgDTAgqqN7jSBrk'
 end
